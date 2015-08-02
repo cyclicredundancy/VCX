4942861e58f1db7979e9c95d9f3f869b92b78720
Vikram D <cyclicredundancy@gmail.com>
2015-08-02 01:03:44 -0700

    added buzzer march to patch feeder v3 u8glib

--- projects/patch_feeder_v3_u8glib/patch_feeder_v3_u8glib.ino
+++ projects/patch_feeder_v3_u8glib/patch_feeder_v3_u8glib.ino
@@ -5,12 +5,15 @@
 #include <U8glib.h>
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);  // I2C / TWI
 
+//#include "pitches.h"
+
 // Components used:
 // Freeduino-V1.22 ATmega328 with FTDI on board
 // ChronoDot
 // SEEED Studio TWIG OLED 128x64 monochrome - with u8glib
 // Parallav continuous rotation servo
 // Adafruit rotary encoder 24 clicks
+// Piezo buzzer
 
 #define encoder0PinA  2
 #define encoder0PinB  7
@@ -42,8 +45,13 @@ bool testing = false;
 
 Servo myServo;
 
+#define buzzer_pin 10
+void march(unsigned speakerPin);
+
 void setup()
 {
+  if (testing) march(buzzer_pin);
+
   pinMode(led, OUTPUT);
 
   pinMode(encoder0PinA, INPUT);
@@ -105,12 +113,14 @@ void setup()
 
 void loop()
 {
-  Serial.print ("pu:");
-  Serial.print (push, DEC);
-  Serial.print (" cli:");
-  Serial.print (clicks, DEC);
-  Serial.print (" cla:");
-  Serial.println (clacks, DEC);
+  if (!(second % 20)) {
+    Serial.print ("pu:");
+    Serial.print (push, DEC);
+    Serial.print (" cli:");
+    Serial.print (clicks, DEC);
+    Serial.print (" cla:");
+    Serial.println (clacks, DEC);
+  }
 
   //int sensorValue = analogRead(sensorPin);
   //clearLine(5);
@@ -152,6 +162,8 @@ void loop()
 
 void dispense()
 {
+  march(buzzer_pin);
+  
   myServo.attach(contiServoPin);
   myServo.writeMicroseconds(1500);  // Stop rotating
 
@@ -324,5 +336,153 @@ void draw(void) {
   u8g.drawStr( 0,  44, cbuf);
  
   u8g.drawStr( 0, 60, " PatchFeeder V3.x");
+}
+
+/////////////////////////////////////////////////////////
+// Imperial March on Buzzer
+/////////////////////////////////////////////////////////
+
+#define c 261
+#define d 294
+#define e 329
+#define f 349
+#define g 391
+#define gS 415
+#define a 440
+#define aS 455
+#define b 466
+#define cH 523
+#define cSH 554
+#define dH 587
+#define dSH 622
+#define eH 659
+#define fH 698
+#define fSH 740
+#define gH 784
+#define gSH 830
+#define aH 880
+//frequencies for the tones we're going to use
+//used http://home.mit.bme.hu/~bako/tonecalc/tonecalc.htm to get these
+
+void beep (unsigned char speakerPin, int frequencyInHertz, long timeInMilliseconds)
+{
+  //digitalWrite(ledPin, HIGH);
+  //use led to visualize the notes being played
+
+  int x;
+  long delayAmount = (long)(1000000 / frequencyInHertz);
+  long loopTime = (long)((timeInMilliseconds * 1000) / (delayAmount * 2));
+  for (x = 0; x < loopTime; x++)
+  {
+    digitalWrite(speakerPin, HIGH);
+    delayMicroseconds(delayAmount);
+    digitalWrite(speakerPin, LOW);
+    delayMicroseconds(delayAmount);
+  }
+
+  //digitalWrite(ledPin, LOW);
+  //set led back to low
+
+  delay(20);
+  //a little delay to make all notes sound separate
+}
+
+void march(unsigned speakerPin)
+{
+  //sets the speakerPin to be an output
+  pinMode(speakerPin, OUTPUT);
+
+  //for the sheet music see:
+  //http://www.musicnotes.com/sheetmusic/mtd.asp?ppn=MN0016254
+  //this is just a translation of said sheet music to frequencies / time in ms
+  //used 500 ms for a quart note
+
+  beep(speakerPin, a, 500);
+  beep(speakerPin, a, 500);
+  beep(speakerPin, a, 500);
+  beep(speakerPin, f, 350);
+  beep(speakerPin, cH, 150);
+
+  beep(speakerPin, a, 500);
+  beep(speakerPin, f, 350);
+  beep(speakerPin, cH, 150);
+  beep(speakerPin, a, 1000);
+  //first bit
+
+  beep(speakerPin, eH, 500);
+  beep(speakerPin, eH, 500);
+  beep(speakerPin, eH, 500);
+  beep(speakerPin, fH, 350);
+  beep(speakerPin, cH, 150);
+
+  beep(speakerPin, gS, 500);
+  beep(speakerPin, f, 350);
+  beep(speakerPin, cH, 150);
+  beep(speakerPin, a, 1000);
+  //second bit...
+
+  beep(speakerPin, aH, 500);
+  beep(speakerPin, a, 350);
+  beep(speakerPin, a, 150);
+  beep(speakerPin, aH, 500);
+  beep(speakerPin, gSH, 250);
+  beep(speakerPin, gH, 250);
+
+  beep(speakerPin, fSH, 125);
+  beep(speakerPin, fH, 125);
+  beep(speakerPin, fSH, 250);
+  delay(250);
+  beep(speakerPin, aS, 250);
+  beep(speakerPin, dSH, 500);
+  beep(speakerPin, dH, 250);
+  beep(speakerPin, cSH, 250);
+  //start of the interesting bit
+
+  beep(speakerPin, cH, 125);
+  beep(speakerPin, b, 125);
+  beep(speakerPin, cH, 250);
+  delay(250);
+  beep(speakerPin, f, 125);
+  beep(speakerPin, gS, 500);
+  beep(speakerPin, f, 375);
+  beep(speakerPin, a, 125);
+
+  beep(speakerPin, cH, 500);
+  beep(speakerPin, a, 375);
+  beep(speakerPin, cH, 125);
+  beep(speakerPin, eH, 1000);
+  //more interesting stuff (this doesn't quite get it right somehow)
+
+  beep(speakerPin, aH, 500);
+  beep(speakerPin, a, 350);
+  beep(speakerPin, a, 150);
+  beep(speakerPin, aH, 500);
+  beep(speakerPin, gSH, 250);
+  beep(speakerPin, gH, 250);
+
+  beep(speakerPin, fSH, 125);
+  beep(speakerPin, fH, 125);
+  beep(speakerPin, fSH, 250);
+  delay(250);
+  beep(speakerPin, aS, 250);
+  beep(speakerPin, dSH, 500);
+  beep(speakerPin, dH, 250);
+  beep(speakerPin, cSH, 250);
+  //repeat... repeat
+
+  beep(speakerPin, cH, 125);
+  beep(speakerPin, b, 125);
+  beep(speakerPin, cH, 250);
+  delay(250);
+  beep(speakerPin, f, 250);
+  beep(speakerPin, gS, 500);
+  beep(speakerPin, f, 375);
+  beep(speakerPin, cH, 125);
+
+  beep(speakerPin, a, 500);
+  beep(speakerPin, f, 375);
+  beep(speakerPin, c, 125);
+  beep(speakerPin, a, 1000);
+  //and we're done \ó/
 }
 
